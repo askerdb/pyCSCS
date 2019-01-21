5089ea82ce7ca2590614ad6aa5d7bb5b1ee543c1
Asker Brejnrod <askerbrejnrod@gmail.com>
2019-01-21 18:04:55 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyCSCS",
-    version="0.0.2",
+    version="0.0.4",
     author="Asker Brejnrod, Piotr Dworzynski",
     author_email="brejnrod@sund.ku.dk",
     description="A python implementation of the CSCS distance",
